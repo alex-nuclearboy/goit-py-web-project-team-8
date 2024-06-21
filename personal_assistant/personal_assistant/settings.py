9536c7d3c8a6788a0ef4510736e1cb18f300ec20
Aleksander <alex.nuclearboy@gmail.com>
2024-06-21 11:19:31 +0200

    Add new applications to the INSTALLED_APPS constant

--- personal_assistant/personal_assistant/settings.py
+++ personal_assistant/personal_assistant/settings.py
@@ -42,6 +42,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'notesapp',
+    'contactsapp',
+    'filemanagerapp',
+    'newsapp',
+    'users',
 ]
 
 MIDDLEWARE = [
