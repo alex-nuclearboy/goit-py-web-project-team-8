cee4b95370cf4eecbf5adde2609f373a484fe272
Aleksander <alex.nuclearboy@gmail.com>
2024-06-21 10:45:44 +0200

    Add variables to send emails

--- personal_assistant/personal_assistant/settings.py
+++ personal_assistant/personal_assistant/settings.py
@@ -159,3 +159,14 @@ MEDIA_URL = '/media/'
 AVATAR_STORAGE = 'users.storage_avatar.LocalStorage'
 
 
+# Email backend configuration for sending emails via SMTP
+# Email server configuration parameters, loaded from environment variables
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = env('EMAIL_HOST')
+EMAIL_PORT = env('EMAIL_PORT')
+EMAIL_STARTTLS = False
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
