c61540d53652102d5bd94e35a7586b391094e4b9
Aleksander <alex.nuclearboy@gmail.com>
2024-06-23 16:09:27 +0200

    Update routes

--- personal_assistant/users/urls.py
+++ personal_assistant/users/urls.py
@@ -9,31 +9,4 @@ urlpatterns = [
     path('login/', views.loginuser, name='login'),
     path('logout/', views.logoutuser, name='logout'),
     path('profile/', views.profile, name='profile'),
-    path(
-        'reset-password/',
-        views.password_reset_request,
-        name='password_reset'
-    ),
-    path(
-        'reset-password/done/',
-        views.CustomPasswordResetDoneView.as_view(
-            template_name='users/password_reset_done.html'
-        ),
-        name='password_reset_done'
-    ),
-    path(
-        'reset-password/confirm/<uidb64>/<token>/',
-        views.CustomPasswordResetConfirmView.as_view(
-            template_name='users/password_reset_confirm.html',
-            success_url='/users/reset-password/complete/'
-        ),
-        name='password_reset_confirm'
-    ),
-    path(
-        'reset-password/complete/',
-        views.CustomPasswordResetCompleteView.as_view(
-            template_name='users/password_reset_complete.html'
-        ),
-        name='password_reset_complete'
-    ),
 ]
