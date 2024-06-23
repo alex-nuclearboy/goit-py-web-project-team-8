c405c50f3051aab4d204e33a70987c2edf5516f0
Aleksander <alex.nuclearboy@gmail.com>
2024-06-23 21:02:47 +0200

    Update localisation for password reset pages

--- personal_assistant/users/forms.py
+++ personal_assistant/users/forms.py
@@ -3,7 +3,8 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import (
     UserCreationForm,
     AuthenticationForm,
-    PasswordResetForm
+    PasswordResetForm,
+    SetPasswordForm
 )
 from django.contrib.auth import authenticate
 from django.contrib.auth.password_validation import (
@@ -16,7 +17,21 @@ from .translations import translations
 
 
 class RegisterForm(UserCreationForm):
+    """
+    Custom user registration form.
+    - Initializes the form with translated labels and placeholders.
+    - Validates username uniqueness.
+    - Validates password confirmation.
+    - Provides custom password validation with translated error messages.
+    """
     def __init__(self, *args, **kwargs):
+        """
+        Initialize of the registration form.
+        - Sets the language and translations.
+        - Updates form field attributes with translated placeholders.
+        - Updates form field labels with translated text.
+        - Updates error messages with translated text.
+        """
         self.language = kwargs.pop('language', 'en')
         self.trans = translations.get(self.language, translations['en'])
         super(RegisterForm, self).__init__(*args, **kwargs)
@@ -47,12 +62,24 @@ class RegisterForm(UserCreationForm):
         })
 
     def clean_username(self):
+        """
+        Validate of the username uniqueness.
+        - Checks if a user with the same username already exists.
+        - Raises a validation error if the username is already taken.
+        - Returns the cleaned username if it is unique.
+        """
         username = self.cleaned_data.get('username')
         if User.objects.filter(username=username).exists():
             raise forms.ValidationError(self.error_messages['username_exists'])
         return username
 
     def clean_password2(self):
+        """
+        Validate password confirmation.
+        - Ensures the two password fields match.
+        - Raises a validation error if the passwords do not match.
+        - Returns the cleaned password confirmation if they match.
+        """
         password1 = self.cleaned_data.get('password1')
         password2 = self.cleaned_data.get('password2')
         if password1 and password2 and password1 != password2:
@@ -62,10 +89,20 @@ class RegisterForm(UserCreationForm):
         return password2
 
     def _post_clean(self):
+        """
+        Perform additional password validation after the form is cleaned.
+        - Validates the password using the default password validators.
+        - Adds custom error messages for password validation errors.
+        """
         super()._post_clean()
         self._validate_custom_password()
 
     def _validate_custom_password(self):
+        """
+        Custom password validation.
+        - Validates the password using the default password validators.
+        - Adds custom error messages for password validation errors.
+        """
         password = self.cleaned_data.get('password1')
         if password:
             errors = []
@@ -93,7 +130,19 @@ class RegisterForm(UserCreationForm):
 
 
 class LoginForm(AuthenticationForm):
+    """
+    Custom user login form.
+    - Initializes the form with translated labels and placeholders.
+    - Authenticates the user based on provided credentials.
+    - Provides error messages for invalid login attempts and inactive accounts.
+    """
     def __init__(self, *args, **kwargs):
+        """
+        Initialize the login form.
+        - Sets the language and translations.
+        - Updates form field attributes with translated placeholders.
+        - Updates form field labels with translated text.
+        """
         self.language = kwargs.pop('language', 'en')
         self.trans = translations.get(self.language, translations['en'])
         super(LoginForm, self).__init__(*args, **kwargs)
@@ -109,10 +158,13 @@ class LoginForm(AuthenticationForm):
         self.fields['username'].label = self.trans['username']
         self.fields['password'].label = self.trans['password']
 
-    class Meta:
-        fields = ['username', 'password']
-
     def clean(self):
+        """
+        Validate login credentials.
+        - Authenticates the user based on the provided username and password.
+        - Raises a validation error if the credentials are invalid
+          or the account is inactive.
+        """
         username = self.cleaned_data.get('username')
         password = self.cleaned_data.get('password')
 
@@ -133,10 +185,21 @@ class LoginForm(AuthenticationForm):
         return self.cleaned_data
 
     def get_user(self):
+        """
+        Get the authenticated user.
+        - Returns the user object if authentication was successful.
+        """
         return self.user_cache
+
+    class Meta:
+        fields = ['username', 'password']
 
 
 class ProfileForm(forms.ModelForm):
+    """
+    Custom user profile form.
+    - Handles the avatar upload for the user's profile.
+    """
     avatar = forms.ImageField(widget=forms.FileInput())
 
     class Meta:
@@ -144,9 +207,104 @@ class ProfileForm(forms.ModelForm):
         fields = ['avatar']
 
 
+class CustomSetPasswordForm(SetPasswordForm):
+    """
+    Custom form for setting a new password.
+    - Initializes the form with translated labels and placeholders.
+    - Validates the new password and its confirmation.
+    - Custom password validation with translated error messages.
+    """
+    def __init__(self, *args, **kwargs):
+        """
+        Initialize the set password form.
+        - Sets the language and translations.
+        - Updates form field attributes with translated placeholders.
+        - Updates form field labels with translated text.
+        """
+        self.language = kwargs.pop('language', 'en')
+        self.trans = translations.get(self.language, translations['en'])
+        super(CustomSetPasswordForm, self).__init__(*args, **kwargs)
+        self.fields['new_password1'].widget.attrs.update({
+            'placeholder': self.trans['enter_new_password'],
+            'class': 'form-control'
+        })
+        self.fields['new_password2'].widget.attrs.update({
+            'placeholder': self.trans['confirm_new_password'],
+            'class': 'form-control'
+        })
+        self.fields['new_password1'].label = self.trans['new_password1']
+        self.fields['new_password2'].label = self.trans['new_password2']
+
+    def clean_new_password2(self):
+        """
+        Validate new password confirmation.
+        - Ensures the two new password fields match.
+        - Raises a validation error if the passwords do not match.
+        - Returns the cleaned password confirmation if they match.
+        """
+        password1 = self.cleaned_data.get('new_password1')
+        password2 = self.cleaned_data.get('new_password2')
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError(
+                self.trans['password_mismatch'],
+                code='password_mismatch',
+            )
+        return password2
+
+    def _post_clean(self):
+        """
+        Perform additional password validation after the form is cleaned.
+        - Validates the password using the default password validators.
+        - Adds custom error messages for password validation errors.
+        """
+        super()._post_clean()
+        self._validate_custom_password()
+
+    def _validate_custom_password(self):
+        """
+        Custom password validation.
+        - Validates the password using the default password validators.
+        - Adds custom error messages for password validation errors.
+        """
+        password = self.cleaned_data.get('new_password1')
+        if password:
+            errors = []
+            try:
+                validate_password(
+                    password, self.user,
+                    password_validators=get_default_password_validators()
+                )
+            except forms.ValidationError as e:
+                for message in e.messages:
+                    if 'too short' in message:
+                        errors.append(self.trans['password_too_short'])
+                    elif 'too common' in message:
+                        errors.append(self.trans['password_too_common'])
+                    elif 'entirely numeric' in message:
+                        errors.append(self.trans['password_entirely_numeric'])
+                    else:
+                        errors.append(message)
+            if errors:
+                self.add_error('new_password1', forms.ValidationError(errors))
+
+
 class CustomPasswordResetForm(PasswordResetForm):
-    email = forms.EmailField(
-        max_length=254,
-        required=True,
-        widget=forms.EmailInput(),
-    )
+    """
+    Custom form for password reset.
+    """
+    def __init__(self, *args, **kwargs):
+        """
+        Initialize the password reset form.
+        - Sets the language and translations.
+        - Updates form field attributes with translated placeholders.
+        - Updates form field labels with translated text.
+        """
+        self.language = kwargs.pop('language', 'en')
+        self.trans = translations.get(self.language, translations['en'])
+        super(CustomPasswordResetForm, self).__init__(*args, **kwargs)
+        self.fields['email'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': self.trans['enter_email'],
+            'autofocus': True
+        })
+        self.fields['email'].label = self.trans['email']
--- personal_assistant/users/views.py
+++ personal_assistant/users/views.py
@@ -1,12 +1,25 @@
+from smtplib import SMTPException
 from django.contrib import messages
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
+from django.contrib.auth.views import (
+    PasswordResetView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView,
+    PasswordResetDoneView
+)
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from django.views.generic.base import ContextMixin
 
 from .forms import (
     RegisterForm,
     LoginForm,
-    ProfileForm
+    ProfileForm,
+    CustomPasswordResetForm,
+    CustomSetPasswordForm
 )
 from .translations import translations
 
@@ -14,7 +27,9 @@ from .translations import translations
 def get_language(request):
     """
     Get the language from the request or session.
-    If not found, default to English.
+    - Retrieves the language from the GET parameters or session.
+    - Defaults to English if no language is found.
+    - Stores the language in the session.
     """
     language = request.GET.get('lang')
     if not language:
@@ -141,4 +156,197 @@ def profile(request):
     return render(
         request, 'users/profile.html',
         context={'profile_form': profile_form, 'translations': trans}
+    )
+
+
+class TranslationsContextMixin(ContextMixin):
+    """
+    Mixin to add translations to the context.
+    - Retrieves the language from the session.
+    - Adds translations to the context based on the language.
+    """
+    def get_context_data(self, **kwargs):
+        """
+        Add translations to the context.
+        - Retrieves the language from the request.
+        - Adds translations to the context based on the language.
+        """
+        context = super().get_context_data(**kwargs)
+        language = get_language(self.request)
+        context['translations'] = translations.get(language,
+                                                   translations['en'])
+        return context
+
+
+class CustomPasswordResetView(
+    SuccessMessageMixin,
+    TranslationsContextMixin,
+    PasswordResetView
+):
+    """
+    Custom password reset view.
+    - Uses custom password reset form.
+    - Sends password reset email with translations.
+    - Displays a success message upon sending the email.
+    """
+    template_name = 'users/password_reset.html'
+    email_template_name = 'users/password_reset_email.html'
+    html_email_template_name = 'users/password_reset_email.html'
+    success_url = reverse_lazy('users:password_reset_done')
+    subject_template_name = 'users/password_reset_subject.txt'
+    form_class = CustomPasswordResetForm
+
+    def get_success_message(self, cleaned_data):
+        """
+        Get the success message for password reset email sent.
+        - Retrieves the translated success message.
+        """
+        language = get_language(self.request)
+        trans = translations.get(language, translations['en'])
+        return trans['email_sent'] % {'email': cleaned_data['email']}
+
+    def get_form_kwargs(self):
+        """
+        Get the form keyword arguments.
+        - Adds the language to the form kwargs.
+        """
+        kwargs = super().get_form_kwargs()
+        kwargs['language'] = get_language(self.request)
+        return kwargs
+
+    def form_valid(self, form):
+        """
+        Handle a valid form submission.
+        - Sends the password reset email.
+        - Displays appropriate error messages if email sending fails.
+        """
+        language = get_language(self.request)
+        trans = translations.get(language, translations['en'])
+        email = form.cleaned_data['email']
+        associated_users = User.objects.filter(email=email)
+        if associated_users.exists():
+            try:
+                return super().form_valid(form)
+            except SMTPException:
+                messages.error(self.request, trans['email_send_error'])
+                return render(
+                    self.request,
+                    self.template_name, {'form': form, 'translations': trans}
+                )
+        else:
+            messages.error(self.request, trans['password_reset_no_account'])
+            return render(
+                self.request,
+                self.template_name, {'form': form, 'translations': trans}
+            )
+
+
+class CustomPasswordResetConfirmView(
+    SuccessMessageMixin,
+    TranslationsContextMixin,
+    PasswordResetConfirmView
+):
+    """
+    Custom password reset confirm view.
+    - Uses custom set password form.
+    - Displays a success message upon successful password reset.
+    - Handles token validation and displays error for invalid tokens.
+    """
+    template_name = 'users/password_reset_confirm.html'
+    form_class = CustomSetPasswordForm
+    success_url = reverse_lazy('users:password_reset_complete')
+
+    def get_form_kwargs(self):
+        """
+        Get the form keyword arguments.
+        - Adds the language to the form kwargs.
+        """
+        kwargs = super().get_form_kwargs()
+        kwargs['language'] = get_language(self.request)
+        return kwargs
+
+    def form_valid(self, form):
+        """
+        Handle a valid form submission.
+        - Displays a success message upon successful password reset.
+        """
+        language = get_language(self.request)
+        trans = translations.get(language, translations['en'])
+        messages.success(
+            self.request, trans['password_reset_complete_message']
+        )
+        return super().form_valid(form)
+
+
+class CustomPasswordResetCompleteView(
+    SuccessMessageMixin,
+    TranslationsContextMixin,
+    PasswordResetCompleteView
+):
+    """
+    Custom password reset complete view.
+    - Displays the password reset complete page.
+    """
+    template_name = 'users/password_reset_complete.html'
+
+
+class CustomPasswordResetDoneView(
+    SuccessMessageMixin,
+    TranslationsContextMixin,
+    PasswordResetDoneView
+):
+    """
+    Custom password reset done view.
+    - Displays the password reset done page.
+    """
+    template_name = 'users/password_reset_done.html'
+
+
+def password_reset_request(request):
+    """
+    Handle password reset request.
+    - If the request method is POST, validate and process
+      the password reset form.
+    - If the form is valid, send the password reset email.
+    - If the email sending fails, display an error message.
+    - If the email is not associated with any account,
+      display an error message.
+    - Re-render the password reset page with the form and translations.
+    """
+    language = get_language(request)
+    trans = translations.get(language, translations['en'])
+
+    if request.method == "POST":
+        form = CustomPasswordResetForm(request.POST, language=language)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            associated_users = User.objects.filter(email=email)
+            if associated_users.exists():
+                try:
+                    return CustomPasswordResetView.as_view()(request)
+                except SMTPException as err:
+                    print(f"SMTP error occurred: {err}")
+                    messages.error(
+                        request,
+                        trans['smtp_error']
+                    )
+                    return redirect('users:password_reset')
+            else:
+                messages.error(
+                    request,
+                    trans['password_reset_no_account']
+                )
+                return render(
+                    request, "users/password_reset.html",
+                    {"form": form, "translations": trans}
+                )
+        else:
+            for error in form.errors.values():
+                messages.error(request, error)
+    else:
+        form = CustomPasswordResetForm(language=language)
+
+    return render(
+        request, "users/password_reset.html",
+        {"form": form, "translations": trans}
     )
