ff9c756ee5a0482ba1917b01245f019f9c7333dc
Oleksandr_Skuz <138138803+OleksandrSkuz1@users.noreply.github.com>
2024-06-24 16:54:23 +0300

    update

--- personal_assistant/notesapp/models.py
+++ personal_assistant/notesapp/models.py
@@ -5,9 +5,13 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Tag(models.Model):
-    name = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=50)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
 
     objects = models.Manager()
+
+    class Meta:
+        constraints = [models.UniqueConstraint(fields=['user', 'name'], name='tag_of_user')]
 
     def __str__(self):
         return self.name
@@ -17,13 +21,12 @@ class Note(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
     tags = models.ManyToManyField(Tag, related_name='notes')
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notes')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
 
     objects = models.Manager()
 
+    class Meta:
+        constraints = [models.UniqueConstraint(fields=['user', 'title'], name='note_of_user')]
+
     def __str__(self):
         return self.title
-
-
-    class Meta:
-        constraints = [models.UniqueConstraint(fields=['user', 'name'], name='tag_of_user')]
