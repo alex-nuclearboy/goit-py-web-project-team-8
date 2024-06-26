929302a4e4a12f8aa60a3ebdd357d329e3171223
Aleksander <alex.nuclearboy@gmail.com>
2024-06-26 20:30:30 +0200

    Update routes

--- personal_assistant/notesapp/urls.py
+++ personal_assistant/notesapp/urls.py
@@ -4,8 +4,11 @@ from . import views
 app_name = 'notesapp'
 
 urlpatterns = [
-    path('', views.note_list, name='note_list'),
-    path('add/', views.add_note, name='add_note'),
-    path('edit/<int:id>/', views.edit_note, name='edit_note'),
-    path('delete/<int:id>/', views.delete_note, name='delete_note'),
+    path('notes/', views.note_list, name='note_list'),
+    path('add_note/', views.add_note, name='add_note'),
+    path('tags/', views.tag_list, name='tag_list'),
+    path('add_tag/', views.add_tag, name='add_tag'),
+    path('note/<int:id>/edit', views.edit_note, name='edit_note'),
+    path('note/<int:id>/delete', views.delete_note, name='delete_note'),
+    path('tag/<int:id>/delete', views.delete_tag, name='delete_tag'),
 ]
