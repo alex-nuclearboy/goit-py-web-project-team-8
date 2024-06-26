b6322e789a1e99ec8dc6b19f19b02d82828278eb
Aleksander <alex.nuclearboy@gmail.com>
2024-06-26 18:36:14 +0200

    Update of forms
    - Add a form for tags
    - Define the title and content fields in the note form

--- personal_assistant/notesapp/forms.py
+++ personal_assistant/notesapp/forms.py
@@ -1,7 +1,18 @@
 from django import forms
 from .models import Note, Tag
 
+
+class TagForm(forms.ModelForm):
+    name = forms.CharField(min_length=3, max_length=25, required=True, widget=forms.TextInput())
+
+    class Meta:
+        model = Tag
+        fields = ['name']
+
+
 class NoteForm(forms.ModelForm):
+    title = forms.CharField(min_length=5, max_length=255, required=True, widget=forms.TextInput())
+    content = forms.CharField(min_length=5, max_length=255, required=False, widget=forms.Textarea(attrs={'rows': 4}))
     tags = forms.ModelMultipleChoiceField(
         queryset=Tag.objects.all(),
         widget=forms.CheckboxSelectMultiple,
@@ -11,4 +22,3 @@ class NoteForm(forms.ModelForm):
     class Meta:
         model = Note
         fields = ['title', 'content', 'tags']
-
