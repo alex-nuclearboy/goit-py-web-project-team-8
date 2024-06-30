2ac21a293f98fa1bab2b79671e1eec61fb1986d3
GievskiyIgor <121798393+GievskiyIgor@users.noreply.github.com>
2024-07-01 01:44:10 +0300

    sort_file_1_01
    work file, file list
    work Category

--- personal_assistant/filemanagerapp/models.py
+++ personal_assistant/filemanagerapp/models.py
@@ -1,3 +1,90 @@
 from django.db import models
+from cloudinary.models import CloudinaryField, Category
+from django.contrib.auth.models import User
+from django import forms
 
 # Create your models here.
+
+# Советы Саши
+# class Category(models.Model): 
+#     name 
+#     user
+    
+    
+# class UploadedFile(models.Model):
+#     category FK Category
+#     file
+#     url adress file
+#     datetime
+#     user
+
+
+class UserFile(models.Model):
+    
+    """
+    Represents the structure of working with user files.
+    
+    Attributes:
+    user: The user who owns the tag.
+    name (str): tag name up to 50 characters long.
+    category (choices=CATEGORY_CHOICES): The default category for user.
+    file: user file 
+    
+    Methods:
+    __str__(): Returns the name file for user.
+    
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    file = models.FileField(upload_to='uploads/')
+    public_id = models.CharField(max_length=255, blank=True)
+        
+    objects = models.Manager()
+
+    class Meta:
+        constraints = [models.UniqueConstraint(fields=['user', 'name', 'category', 'file'], name='file_of_user')]
+
+    def __str__(self):
+        return self.name    
+    
+
+class FileUploadForm(forms.ModelForm):
+    
+    """
+    Represents the file upload structure
+    
+    """
+    category = forms.ModelChoiceField(queryset=Category.objects.all(), required=True)
+
+    class Meta:
+        model = UserFile
+        fields = ['file', 'category', 'name']  
+        
+
+class Category(models.Model):
+    
+    """
+    Represents the structure category.
+    
+    CATEGORY_CHOICES - Standard categories for all users
+
+    Attributes:
+    name (str): tag name up to 50 characters long.
+    is_standard (choices=CATEGORY_CHOICES): The default category for user.
+        
+    Methods:
+    __str__(): Returns the name file for user.
+    
+    """
+    STANDARD_CATEGORIES = [
+        ('image', 'Image'),
+        ('document', 'Document'),
+        ('video', 'Video'),
+    ]
+
+    name = models.CharField(max_length=255, unique=True)
+    is_standard = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
--- personal_assistant/filemanagerapp/views.py
+++ personal_assistant/filemanagerapp/views.py
@@ -1,3 +1,126 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import UserFile, FileUploadForm, Category
+from .forms import CategoryForm
+from cloudinary.uploader import upload, destroy
 
 # Create your views here.
+# функция для за вантаження файлу + 
+# отображення функция списку файлов + 
+
+# скачать файл +
+# удалить файл +
+# 
+# функция для створення сатегории +
+# удалить +
+
+# Work file
+def upload_file(request):
+    
+    """
+    View function for uploading files to a cloud service list of uploaded files by user
+    
+    Returns:
+        Message about successful or unsuccessful uploaded of user file
+    """
+    
+    if request.method == 'POST':
+        form = FileUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            uploaded_file = upload(request.FILES['file'])
+            user_file = form.save(commit=False)
+            user_file.public_id = uploaded_file['public_id']
+            user_file.save()
+            return redirect('file_list')
+    else:
+        form = FileUploadForm()
+    return render(request, 'upload.html', {'form': form})
+    
+    # if request.method == 'POST':
+    #     form = FileUploadForm(request.POST, request.FILES)
+    #     if form.is_valid():
+    #         form.save()
+    #         return redirect('file_list')
+    # else:
+    #     form = FileUploadForm()
+    # return render(request, 'upload.html', {'form': form})           
+
+
+def file_list(request, category=None):
+    
+    """
+    View function for viewing uploaded files to a cloud service
+    
+    Returns:
+    file_list  - list of uploads files by user.
+    
+    """
+    
+    if category:
+        files = UserFile.objects.filter(category=category)
+    else:
+        files = UserFile.objects.all()
+    return render(request, 'file_list.html', {'files': files, 'category': category})
+
+
+def download_file(request, file_id):
+    
+    """
+    View function for downloading files from cloud service
+    
+    Returns:
+    user_file  - User file.
+    
+    """
+    
+    user_file = get_object_or_404(UserFile, id=file_id)
+    
+    return redirect(user_file.file.url)
+
+
+def delete_file(request, file_id):
+    
+    """
+    View function for delete files from cloud service
+    
+    Returns:
+    user_file  - delete user file
+    
+    """
+    
+    user_file = get_object_or_404(UserFile, id=file_id)
+    destroy(user_file.public_id)
+    user_file.delete()
+    return redirect('file_list')
+
+
+# *******************************************************
+# Category
+def manage_category(request):
+    
+    """
+    View function for displaying a list of notes associated with the logged-in user.
+
+    Retrieves notes based on search queries and tag filters, handles pagination.
+
+    Returns:
+    HttpResponse: Rendered template with notes, tags, search form, and tag filter.
+    """
+    
+    if request.method == 'POST':
+        if 'create' in request.POST:
+            form = CategoryForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect('manage_category')
+        elif 'delete' in request.POST:
+            category_id = request.POST.get('category_id')
+            category = get_object_or_404(Category, id=category_id)
+            if not category.is_standard:
+                category.delete()
+            return redirect('manage_category')
+    else:
+        form = CategoryForm()
+
+    categories = Category.objects.all()
+    
+    return render(request, 'manage_category.html', {'categories': categories, 'form': form})
