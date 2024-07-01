32a0d0c9c3bb740c89c22df04d560f43e81c227f
Aleksander <alex.nuclearboy@gmail.com>
2024-07-01 17:23:12 +0200

    Update the routes

--- personal_assistant/contactsapp/urls.py
+++ personal_assistant/contactsapp/urls.py
@@ -4,13 +4,12 @@ from . import views
 app_name = "contactsapp"
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("contacts/", views.my_contacts, name="contacts"),
+    path("contacts/", views.my_contacts, name="contact_list"),
     path("contact/add", views.ContactCreateView.as_view(), name="add_contact"),
-    path("contact/<int:contact_id>/", views.contact_details, name="contact_details"),
     path("contact/<int:pk>/update", views.ContactUpdateView.as_view(), name="update_contact"),
-    path("contact/<int:pk>/delete", views.ContactDeleteView.as_view(), name="delete_contact"),
-    path("groups/", views.groups, name="groups"),
+    path("contact/<int:contact_id>/", views.contact_details, name="contact_details"),
+    path("contact/<int:pk>/delete", views.delete_contact, name="delete_contact"),
+    path("groups/", views.my_groups, name="group_list"),
     path("group/add/", views.GroupCreateView.as_view(), name="add_group"),
     path("group/<int:pk>/update", views.GroupUpdateView.as_view(), name="update_group"),
     path("group/<int:pk>/delete", views.delete_group, name="delete_group"),
