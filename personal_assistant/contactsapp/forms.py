ec76af574322a2c5e2b3d9f4f5987b6f2cf6d81f
Aleksander <alex.nuclearboy@gmail.com>
2024-07-01 17:18:19 +0200

    Update forms for contacts
    - Added translation of default groups
    - Added a verification for name uniqueness

--- personal_assistant/contactsapp/forms.py
+++ personal_assistant/contactsapp/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 from .models import Contact, Group
 from .localize import text_array as translations
@@ -10,7 +11,7 @@ class ContactForm(forms.ModelForm):
         min_length=3,
         max_length=20,
         required=True,
-        widget=forms.TextInput(attrs={"class": "form-control", "autofocus": "true",})
+        widget=forms.TextInput(attrs={"class": "form-control", "autofocus": "true"})
     )
     phone = forms.CharField(
         min_length=9,
@@ -38,7 +39,8 @@ class ContactForm(forms.ModelForm):
     group = forms.ModelChoiceField(
         queryset=Group.objects.none(),
         required=False,
-        widget=forms.Select(attrs={'class': 'form-select'})
+        widget=forms.Select(attrs={'class': 'form-select'}),
+        empty_label='------'  # This ensures that the group field can be empty
     )
 
     class Meta:
@@ -46,7 +48,7 @@ class ContactForm(forms.ModelForm):
         fields = ["name", "phone", "email", "address", "birthday", "group"]
 
     def __init__(self, *args, **kwargs):
-        user = kwargs.pop('user', None)
+        self.user = kwargs.pop('user', None)
         language = kwargs.pop('language', 'en')
         super().__init__(*args, **kwargs)
         self.trans = translations.get(language, translations['en'])
@@ -61,8 +63,33 @@ class ContactForm(forms.ModelForm):
         self.fields['birthday'].widget.attrs.update({'placeholder': self.trans['birthday_placeholder']})
         self.fields['birthday'].label = self.trans['birthday']
         self.fields['group'].label = self.trans['group']
-        if user is not None:
-            self.fields['group'].queryset = Group.objects.filter(creator=user)
+        if self.user is not None:
+            groups = Group.objects.filter(creator=self.user)
+            translated_groups = []
+            for group in groups:
+                if group.name == 'Family':
+                    group.name = self.trans['family_group']
+                elif group.name == 'Friends':
+                    group.name = self.trans['friends_group']
+                elif group.name == 'Work':
+                    group.name = self.trans['work_group']
+                translated_groups.append(group)
+            self.fields['group'].queryset = Group.objects.filter(creator=self.user)
+            self.fields['group'].choices = [('', '------')] + [(group.id, group.name) for group in translated_groups]
+
+    def clean(self):
+        cleaned_data = super().clean()
+        name = cleaned_data.get('name')
+
+        if self.instance.pk:
+            # Exclude the current instance from the check
+            if Contact.objects.filter(creator=self.user, name=name).exclude(pk=self.instance.pk).exists():
+                raise ValidationError(self.trans['duplicate_contact'])
+        else:
+            if Contact.objects.filter(creator=self.user, name=name).exists():
+                raise ValidationError(self.trans['duplicate_contact'])
+
+        return cleaned_data
 
 
 class GroupForm(forms.ModelForm):
@@ -78,11 +105,29 @@ class GroupForm(forms.ModelForm):
         fields = ['name']
 
     def __init__(self, *args, **kwargs):
+        self.user = kwargs.pop('user', None)
         language = kwargs.pop('language', 'en')
         super().__init__(*args, **kwargs)
         self.trans = translations.get(language, translations['en'])
         self.fields['name'].label = self.trans['group_name']
         self.fields['name'].widget.attrs.update({'placeholder': self.trans['group_name_placeholder']})
+
+        if self.user is not None:
+            self.fields['name'].queryset = Group.objects.filter(creator=self.user)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        name = cleaned_data.get('name')
+
+        if self.instance.pk:
+            # Exclude the current instance from the check
+            if Group.objects.filter(creator=self.user, name=name).exclude(pk=self.instance.pk).exists():
+                raise ValidationError(self.trans['duplicate_group'])
+        else:
+            if Group.objects.filter(creator=self.user, name=name).exists():
+                raise ValidationError(self.trans['duplicate_group'])
+
+        return cleaned_data
 
 
 class ContactSearchForm(forms.Form):
